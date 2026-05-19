c42c56adbd5c4e7cffe32d1465ab4735b8681cf2
ryanchua25 <rchu0025@student.monash.edu>
2026-05-19 11:42:50 +0800

    adding files to github

---
+++ vega_lite_vis.js
@@ -0,0 +1,31 @@
+var chart_1 = "charts/chart_1.json";
+vegaEmbed("#chart_1", chart_1).then(function(result) {
+}).catch(console.error);
+
+var chart_2 = "charts/chart_2.json";
+vegaEmbed("#chart_2", chart_2).then(function(result) {
+}).catch(console.error);
+
+var chart_3 = "charts/chart_3.json";
+vegaEmbed("#chart_3", chart_3).then(function(result) {
+}).catch(console.error);
+
+var chart_4 = "charts/chart_4.json";
+vegaEmbed("#chart_4", chart_4).then(function(result) {
+}).catch(console.error);
+
+var chart_5 = "charts/chart_5.json";
+vegaEmbed("#chart_5", chart_5).then(function(result) {
+}).catch(console.error);
+
+var chart_6 = "charts/chart_6.json";
+vegaEmbed("#chart_6", chart_6).then(function(result) {
+}).catch(console.error);
+
+var chart_7 = "charts/chart_7.json";
+vegaEmbed("#chart_7", chart_7).then(function(result) {
+}).catch(console.error);
+
+var chart_8 = "charts/chart_8.json";
+vegaEmbed("#chart_8", chart_8).then(function(result) {
+}).catch(console.error);
